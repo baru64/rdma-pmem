e352ffeadbd0c740f00faac0adf03082032a016b
Bartosz Krakowiak <bkrakowiak@man.poznan.pl>
2021-08-31 01:42:42 +0200

    update make_charts.py

--- rwbenchmark2/scripts/make_charts.py
+++ rwbenchmark2/scripts/make_charts.py
@@ -1,11 +1,26 @@
 #!/bin/python3
+import json
+
 import matplotlib.pyplot as plt
 
-fig, ax = plt.subplots()
-x = []
-y = []
-ax.plot(x, y, label="benchmark")
-ax.set_xlabel('x label')
-ax.set_ylabel('y label')
-ax.set_title("Benchmarks")
-ax.legend()
+results = {}
+figures = ["throughput", "latency", "jitter"]
+with open("results.json", "r") as f:
+    results: dict = json.load(f)
+for figure in figures:
+    fig, ax = plt.subplots()
+    for benchmark in results.keys():
+        y = [] # values
+        x = [] # threads
+        x = sorted([k for k in results[benchmark].keys()])
+        for thread in x:
+            y.append(results[benchmark][thread].get(figure))
+        ax.plot(x, y, label=benchmark)
+
+    ax.set_xlabel('threads')
+    ax.set_ylabel(figure)
+    ax.set_title(figure)
+    ax.legend()
+    # fig.savefig(f"{figure}.png")
+
+plt.show()
