5565ce605cd549fc2dbf171f2a63e25471d60c31
Bartosz Krakowiak <bkrakowiak@man.poznan.pl>
2021-09-01 10:57:13 +0200

    update make_charts.py

--- rwbenchmark2/scripts/make_charts.py
+++ rwbenchmark2/scripts/make_charts.py
@@ -4,28 +4,51 @@ import json
 import matplotlib.pyplot as plt
 
 all_results = {}
-memsizes = ["512", "1024", "2048", "4092", "8192", "16384"]
+memsizes = ["512", "1024", "2048", "4096", "8192", "16384", "32768", "65536"]
 figures = ["throughput", "latency", "jitter"]
+titles = {"throughput": "throughput", "latency": "avg. latency", "jitter": "avg. jitter"}
+labels = {"throughput": "GB/s", "latency": "ns", "jitter": "ns"}
 with open("results.json", "r") as f:
     all_results: dict = json.load(f)
-fig, ax = plt.subplots(len(memsizes), len(figures))
+
+for thread in ["1", "2", "4", "8", "12"]:
+    for figure in figures:
+        benchmarks = {}
+        for memsize in memsizes:
+            results = all_results[memsize]
+            for benchmark in results.keys():
+                if not benchmark in benchmarks:
+                    benchmarks[benchmark] = []
+                benchmarks[benchmark].append(results[benchmark][thread].get(figure))
+
+        fig, ax = plt.subplots()
+        for benchmark, y in benchmarks.items():
+            x = [int(m) for m in memsizes]
+            ax.plot(x, y, label=benchmark)
+            ax.set_xlabel('bytes')
+            ax.set_ylabel(labels[figure])
+            ax.set_title(f"{titles[figure]}: {thread} connection(s)")
+            ax.legend()
+        fig.tight_layout()
+        fig.savefig(f"results/{figure}_memsize{thread}.pdf")
+
 for j, memsize in enumerate(memsizes):
+    fig, ax = plt.subplots(1, len(figures))
     results = all_results[memsize]
     for i, figure in enumerate(figures):
         for benchmark in results.keys():
             y = [] # values
             x = [] # threads
-            x = sorted([k for k in results[benchmark].keys()])
+            x = sorted([int(k) for k in results[benchmark].keys()])
             for thread in x:
-                y.append(results[benchmark][thread].get(figure))
-            ax[j][i].plot(x, y, label=benchmark)
+                y.append(results[benchmark][str(thread)].get(figure))
+            ax[i].plot(x, y, label=benchmark)
 
-        ax[j][i].set_xlabel('connections')
-        ax[j][i].set_ylabel(figure)
-        ax[j][i].set_title(f"{figure}: {memsize}B message")
+        ax[i].set_xlabel('connections')
+        ax[i].set_ylabel(labels[figure])
+        ax[i].set_title(f"{titles[figure]}: {memsize}B message")
 
-ax[0][0].legend()
-fig.tight_layout()
-# fig.savefig(f"results{memsize}.pdf")
-fig.savefig("results.pdf")
-plt.show()
+    ax[0].legend()
+    fig.tight_layout()
+    fig.savefig(f"results/results{memsize}.pdf")
+    # plt.show()
