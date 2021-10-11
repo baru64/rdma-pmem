656317453878b1e455e89ba9b915f538503b0ba0
Bartosz Krakowiak <bartosz_krakowiak@protonmail.com>
2021-10-11 13:52:48 +0200

    new run_iperf

---
+++ rwbenchmark2/scripts/run_iperf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SERVER_IP=10.10.0.124
+
+for MSIZE in 256 512 1024 2048 4096 8192 12288 16384 20480 24576 32768
+do
+    for THREADS in 1 2 4 8
+    do
+        iperf3 -c $SERVER_IP -l $MSIZE -P $THREADS -t 20 -i 20 -J --logfile ./iperf3-$MSIZE-$THREADS-$1.json 
+    done
+done
